50120ba6a02b584291525a5d360cb57b6bf8c8a7
fdfzcq <fdfzcq@gmail.com>
2020-08-13 21:31:13 +0200

    Create project skeleton

---
+++ src/database.rs
@@ -0,0 +1,6 @@
+pub fn init_db() {
+    println!("initing database...");
+    // TODO: connect to database
+    // TODO: create db/tables if not exist
+    // TODO: run migration scripts if needed    
+}
---
+++ src/main.rs
@@ -0,0 +1,17 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+mod database;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    database::init_db();
+    // TODO: enrich database (separate process)
+    // TODO: more endpoints
+    rocket::ignite().mount("/", routes![index]).launch();
+}
